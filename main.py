ad34e4e148d00b2faf9c56c0339c34b2cebee453
Chongoloka <msukwamose1@gmail.com>
2021-11-08 14:43:05 +0200

    init commit

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 print("This is the start of the function")
+def fun():
+    name =['Mose B Msukwa', 'Mose B Msukwa','Mose B Msukwa','Mose B Msukwa']
+
+    for a in name:
+        print( a)
+
+fun()
